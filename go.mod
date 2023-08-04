51440860c10d01bf656260d50bec031e39a56777
Masih H. Derkani <m@derkani.org>
2023-08-04 13:26:16 +0100

    Add Boost transport and market types

---
+++ go.mod
@@ -0,0 +1,39 @@
+module github.com/masih/boostly
+
+go 1.20
+
+require (
+	github.com/filecoin-project/go-address v1.1.0
+	github.com/filecoin-project/go-state-types v0.12.0
+	github.com/google/uuid v1.3.0
+	github.com/ipfs/go-cid v0.4.1
+	github.com/whyrusleeping/cbor-gen v0.0.0-20230418232409-daab9ece03a0
+	golang.org/x/xerrors v0.0.0-20220907171357-04be3eba64a2
+)
+
+require (
+	github.com/filecoin-project/go-amt-ipld/v4 v4.0.0 // indirect
+	github.com/filecoin-project/go-bitfield v0.2.4 // indirect
+	github.com/filecoin-project/go-crypto v0.0.1 // indirect
+	github.com/filecoin-project/go-hamt-ipld/v3 v3.1.0 // indirect
+	github.com/ipfs/go-block-format v0.1.2 // indirect
+	github.com/ipfs/go-ipfs-util v0.0.3 // indirect
+	github.com/ipfs/go-ipld-cbor v0.0.6 // indirect
+	github.com/ipfs/go-ipld-format v0.5.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.5 // indirect
+	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
+	github.com/minio/sha256-simd v1.0.1 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
+	github.com/multiformats/go-base32 v0.1.0 // indirect
+	github.com/multiformats/go-base36 v0.2.0 // indirect
+	github.com/multiformats/go-multibase v0.2.0 // indirect
+	github.com/multiformats/go-multihash v0.2.3 // indirect
+	github.com/multiformats/go-varint v0.0.7 // indirect
+	github.com/polydawn/refmt v0.89.0 // indirect
+	github.com/smartystreets/assertions v1.13.0 // indirect
+	github.com/spaolacci/murmur3 v1.1.0 // indirect
+	github.com/stretchr/testify v1.8.4 // indirect
+	golang.org/x/crypto v0.11.0 // indirect
+	golang.org/x/sys v0.10.0 // indirect
+	lukechampine.com/blake3 v1.2.1 // indirect
+)
---
+++ market.go
@@ -0,0 +1,93 @@
+package boostly
+
+import (
+	"github.com/filecoin-project/go-address"
+	"github.com/filecoin-project/go-state-types/abi"
+	"github.com/filecoin-project/go-state-types/builtin/v9/market"
+	"github.com/filecoin-project/go-state-types/crypto"
+	"github.com/google/uuid"
+	"github.com/ipfs/go-cid"
+)
+
+//go:generate go run github.com/hannahhoward/cbor-gen-for@latest --map-encoding StorageAsk DealStatusRequest DealStatusResponse DealStatus DealParams Transfer DealResponse
+
+// StorageAsk defines the parameters by which a miner will choose to accept or
+// reject a deal. Note: making a storage deal proposal which matches the miner's
+// ask is a precondition, but not sufficient to ensure the deal is accepted (the
+// storage provider may run its own decision logic).
+type StorageAsk struct {
+	// Price per GiB / Epoch
+	Price         abi.TokenAmount
+	VerifiedPrice abi.TokenAmount
+
+	MinPieceSize abi.PaddedPieceSize
+	MaxPieceSize abi.PaddedPieceSize
+	Miner        address.Address
+}
+
+// DealStatusRequest is sent to get the current state of a deal from a
+// storage provider
+type DealStatusRequest struct {
+	DealUUID  uuid.UUID
+	Signature crypto.Signature
+}
+
+// DealStatusResponse is the current state of a deal
+type DealStatusResponse struct {
+	DealUUID uuid.UUID
+	// Error is non-empty if there is an error getting the deal status
+	// (eg invalid request signature)
+	Error          string
+	DealStatus     *DealStatus
+	IsOffline      bool
+	TransferSize   uint64
+	NBytesReceived uint64
+}
+
+type DealStatus struct {
+	// Error is non-empty if the deal is in the error state
+	Error string
+	// Status is a string corresponding to a deal checkpoint
+	Status string
+	// SealingStatus is the sealing status reported by lotus miner
+	SealingStatus string
+	// Proposal is the deal proposal
+	Proposal market.DealProposal
+	// SignedProposalCid is the cid of the client deal proposal + signature
+	SignedProposalCid cid.Cid
+	// PublishCid is the cid of the Publish message sent on chain, if the deal
+	// has reached the publish stage
+	PublishCid *cid.Cid
+	// ChainDealID is the id of the deal in chain state
+	ChainDealID abi.DealID
+}
+
+type DealParams struct {
+	DealUUID           uuid.UUID
+	IsOffline          bool
+	ClientDealProposal market.ClientDealProposal
+	DealDataRoot       cid.Cid
+	Transfer           Transfer // Transfer params will be the zero value if this is an offline deal
+	RemoveUnsealedCopy bool
+	SkipIPNIAnnounce   bool
+}
+
+// Transfer has the parameters for a data transfer
+type Transfer struct {
+	// The type of transfer eg "http"
+	Type string
+	// An optional ID that can be supplied by the client to identify the deal
+	ClientID string
+	// A byte array containing marshalled data specific to the transfer type
+	// eg a JSON encoded struct { URL: "<url>", Headers: {...} }
+	Params []byte
+	// The size of the data transferred in bytes
+	Size uint64
+}
+
+type DealResponse struct {
+	Accepted bool
+	// Message is the reason the deal proposal was rejected. It is empty if
+	// the deal was accepted.
+	Message string
+}
